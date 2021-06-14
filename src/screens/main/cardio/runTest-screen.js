b6e319ba49e50eaca75c2929a4d521fd24da4aa9
rpsutton <57606697+rpsutton@users.noreply.github.com>
2021-06-13 22:33:32 -0400

    improved timer screen UI, working animation

--- src/screens/main/cardio/runTest-screen.js
+++ src/screens/main/cardio/runTest-screen.js
@@ -11,13 +11,14 @@ import {
   Button,
   Spinner,
 } from '@ui-kitten/components';
-import {View, LogBox, Animated} from 'react-native';
+import {View, Animated, useWindowDimensions} from 'react-native';
 import {CountdownCircleTimer} from 'react-native-countdown-circle-timer';
 import * as Animatable from 'react-native-animatable';
 import Tts from 'react-native-tts';
 import {useRun} from '../../../util/db';
 
 export const RunTestScreen = ({navigation, route}) => {
+  const windowWidth = useWindowDimensions().width;
   const [loading, setLoading] = useState(true);
   const [runSequence, setRunSequence] = useState([]);
   const [key, setKey] = useState(0);
@@ -25,14 +26,29 @@ export const RunTestScreen = ({navigation, route}) => {
   const GoTextAnimationRef = useRef(null);
   const styles = useStyleSheet(themedStyle);
   // initialize audio
-  Tts.addEventListener('tts-start', (event) => null);
-  Tts.addEventListener('tts-finish', (event) => null);
-  Tts.addEventListener('tts-cancel', (event) => null);
+  Tts.addEventListener('tts-start', event => null);
+  Tts.addEventListener('tts-finish', event => null);
+  Tts.addEventListener('tts-cancel', event => null);
 
   const [isPlaying, setIsPlaying] = useState(false);
 
+  // this is not great because the timer resets only when key is altered
+  // resetting means moving the key to 0, but if the key is already at 0 nothing will happen unil onComplete
+  // the quick incrementing of the key enables reset to 0 whem index is already 0, but is poor UX
+  function reset() {
+    if (key === 0 ) {
+      setKey(prevKey => prevKey + 1);
+      setTimeout(() => {
+        setKey(0);
+      }, 200);
+    } else {
+      setKey(0);
+    }
+  }
+
   const {data: items, status} = useRun('bywXlnulWOfiwolwEWfv');
 
+  // format the run data into a flat array
   useEffect(() => {
     if (status === 'success') {
       let formattedRun = [];
@@ -137,25 +153,54 @@ export const RunTestScreen = ({navigation, route}) => {
         />
         <Divider />
         <Layout style={styles.container} level="2">
-          <CountdownCircleTimer
-            onComplete={() => {
-              setKey(key + 1);
-            }}
-            isPlaying={isPlaying}
-            key={key}
-            duration={runSequence[key]}
-            size={300}
-            colors={key % 2 === 0 ? normalColors : restColor}>
-            {({remainingTime, animatedColor}) => (
-              <Animated.Text style={{color: animatedColor}}>
-                {remainingTime}
-              </Animated.Text>
-            )}
-          </CountdownCircleTimer>
+          <View style={{marginBottom: '50%'}}>
+            <CountdownCircleTimer
+              onComplete={() => {
+                setKey(key + 1);
+              }}
+              isPlaying={isPlaying}
+              key={key}
+              duration={runSequence[key]}
+              size={windowWidth * 0.9}
+              strokeWidth={18}
+              colors={key % 2 === 0 ? normalColors : restColor}>
+              {({remainingTime}) => {
+                const minutes = Math.floor(remainingTime / 60);
+                const seconds = remainingTime % 60;
+
+                return (
+                  <Text category="h4" style={{fontSize: 72}}>
+                    {minutes} : {seconds}
+                  </Text>
+                );
+              }}
+            </CountdownCircleTimer>
+          </View>
           {isPlaying ? (
-            <Button onPress={() => setIsPlaying(false)}>stop</Button>
+            <Button
+              size="medium"
+              status="danger"
+              style={styles.pauseButton}
+              onPress={() => setIsPlaying(false)}>
+              Pause
+            </Button>
           ) : (
-            <Button onPress={() => setIsPlaying(true)}>start</Button>
+            <View style={styles.buttonGroup}>
+              <Button
+                size="medium"
+                style={styles.startButton}
+                onPress={() => setIsPlaying(true)}>
+                Start
+              </Button>
+              <Button
+                appearance="outline"
+                status="danger"
+                size="medium"
+                style={styles.resetButton}
+                onPress={() => reset()}>
+                Reset
+              </Button>
+            </View>
           )}
         </Layout>
       </>
@@ -165,7 +210,21 @@ export const RunTestScreen = ({navigation, route}) => {
 const themedStyle = StyleService.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  buttonGroup: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    width: '90%',
+  },
+  startButton: {
+    width: '65%',
+  },
+  pauseButton: {
+    width: '60%',
+  },
+  resetButton: {
+    width: '30%',
   },
 });
